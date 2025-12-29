878ef86e7cbc2400a27c8ce6842004dddc560f59
DC-Diego <dicavallaro.dc@gmail.com>
2025-12-28 21:06:38 -0300

    ADD: Subtitles, search, filter, sort, new task, task areas

--- data.js
+++ data.js
@@ -1,26 +1,64 @@
 "hello, world!" 
 
+const Groups =[];
+const tasks =[];
+const MultiTasks =[];
+
+
+
+class Topic{
+  constructor(name){
+    this.name = name;
+    this.status = 0;
+    // 0 → not done, 1 → done, -1 → cancel
+  }
+
+  setStatus(status){
+    this.status = status;
+  }
+    
+}
+
+
+
 class Group{
   constructor(id, name, desc){
     this.id_group = id; // PK
     this.name_group = name;
     this.desc_group = desc;
-
   }
 
 }
 
 class Task{
-  constructor(id, name, desc, id_group = 0, isDone, creationDate, endDate){
+  constructor(id, name, desc, id_group = 0, status, creationDate, endDate){
     this.id_task = id; // PK
     this.id_group = id_group; // FK - Group(id_group)
     this.name_task = name;
     this.desc_task = desc;
-    this.isDone = isDone;
+    this.status = status;
     this.creationDate_task = creationDate ;
     this.endDate_task = endDate;
-
+    this.topics = [];    
+    //TASK has description, name, group, status, time ....
+    //NOTE: Just a 
   }
+
+  createTopic(name){
+    this.topics.push(new Topic(name));
+  }
+
+  calculateProgress(){
+    let total = 0;
+    let qtd = 0;
+    this.topics.forEach(e=>{
+      total+=e.status!=2;
+      qtd += e.status == 1;
+    });
+    return qtd/total;
+  }
+
+
 }
 
 class MultiTask{
@@ -28,7 +66,6 @@ class MultiTask{
     this.id_multi = id_multi; // PK
     this.id_father = id_father; // FK - Task(id)
     this.id_son = id_son; // FK - Task(id)
-   
 
   }
 
--- script.js
+++ script.js
@@ -1,16 +1,113 @@
 const headerTabs = document.querySelectorAll(".header-tab");
 
+const gradientColors = ["#333333", "#030312","#6e4e22","#bafec0","#320000", "#000000"]; // REMOVE?
 
-headerTabs.forEach(e=>{
+const great = document.querySelector(".great");
+
+/*
+3P == 3points, more options etc...
+#icon = ! if has deadline, !! if near deadline, !!! pendent
+
+|---------------------------|
+| #id    #group  #icon   3P |
+|---------------------------|
+| #title                    |
+| #partOfDescription        |
+| #progress                 |
+|---------------------------|
+
+
+
+
+
+
+*/
+
+
+
+headerTabs.forEach((e,i)=>{
   e.addEventListener('click', ()=>{
     const active = document.querySelector(".active-tab");
     if(e!=active){
       active.classList.remove('active-tab');
       e.classList.add('active-tab');
+      // if(i < gradientColors.length)
+        // great.style.background = `${gradientColors[i]}`;
 
     }
 
 
   });
 
-})
+});
+
+
+function renderTasks(){
+  document.querySelectorAll(".task").forEach(e=>{
+    e.remove();
+  })
+  const idTasks = document.getElementById("id-tasks");
+  for (let i = 0; i < 31; i++) {
+    /*
+    <div class="task Pendent">
+          <div class="task-header">
+            <h1>#1</h1>
+            <h1>group1</h1>
+            <h1>!</h1>
+            </div>
+          <div class="task-body">
+            <h1>Task1 </h1>
+            <h1>Lorem ipsum its not lorem ipsum, im writing random text as description, so it may work</h1>
+            <h1>percent completed: 1% </h1>
+            
+          </div>
+        </div>
+    */
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");    
+    taskDiv.classList.add("notStarted");    
+
+    const header = document.createElement("div");
+    header.classList.add("task-header");     
+
+    taskDiv.appendChild(header);
+
+    const id = document.createElement("h1");
+    const group = document.createElement("h1");
+    // const divIcon = document.createElement("div");
+    id.innerText="#"+i;
+    group.innerText="Group "+i;
+    header.appendChild(id);
+    header.appendChild(group);
+
+    const taskBody = document.createElement("div");
+    taskBody.classList.add("task-body");      
+    taskDiv.appendChild(taskBody);
+
+    const title = document.createElement("h1");
+    const desc = document.createElement("h1");
+    const percent = document.createElement("h1");
+    title.innerText = "Title "+i;
+    desc.innerText = "Lorem ipsum its not lorem ipsum, im writing random text as descriptio... "+i;
+    percent.innerText = "percent completed: "+i+"%";
+    
+    taskBody.appendChild(title);
+    taskBody.appendChild(desc);
+    taskBody.appendChild(percent);
+    
+    idTasks.appendChild(taskDiv);
+
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+renderTasks();
